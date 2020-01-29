8983a6127ce5f602fe1c1a2d0032c447f1934042
Charles Pelton <charliejp0311@gmail.com>
2020-01-29 20:08:31 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -46,7 +46,9 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  holiday_hash[:winter].values
+  holiday_hash[:winter].each do |holiday,supplies|
+    holiday.flatten
+  end
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -57,7 +59,12 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  #hol = []
+  #hol = holiday.to_s.split("_")
+  #hol.each do |word|
+  #  word.capitalize!
+  #end
+  
 end
 
 def all_holidays_with_bbq(holiday_hash)
